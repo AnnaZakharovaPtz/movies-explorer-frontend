5ce3da0c0c0c18657e20cce46034d51db4b69c88
Anna Zakharova <palmiraptz89@yandex.ru>
2023-08-31 14:35:01 +0300

    fix: Add section for SeacrhForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,31 +2,33 @@ import './SearchForm.css';
 
 function SearchForm() {
   return (
-    <form className='search-form'>
-      <div className='search-form__container'>
-        <input
-          className="search-form__input"
-          type="text"
-          id="search-input"
-          name="search"
-          placeholder="Фильм"
-          minLength="2"
-          maxLength="50"
-          required
-        />
-        <button className="search-form__submit" type="submit">Найти</button>
-      </div>
-      <label className='search-form__checkbox-container'>
-        <input
-          className='search-form__input'
-          type="checkbox"
-          id="short-film"
-          name="short-film"
-        />
-        <span className='search-form__checkmark' />
-        Короткометражки
-      </label>
-    </form>
+    <section>
+      <form className='search-form'>
+        <div className='search-form__container'>
+          <input
+            className="search-form__input"
+            type="text"
+            id="search-input"
+            name="search"
+            placeholder="Фильм"
+            minLength="2"
+            maxLength="50"
+            required
+          />
+          <button className="search-form__submit" type="submit">Найти</button>
+        </div>
+        <label className='search-form__checkbox-container'>
+          <input
+            className='search-form__input'
+            type="checkbox"
+            id="short-film"
+            name="short-film"
+          />
+          <span className='search-form__checkmark' />
+          Короткометражки
+        </label>
+      </form>
+    </section>
   );
 }
 
